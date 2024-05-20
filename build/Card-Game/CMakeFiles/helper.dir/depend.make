880c48c39e90104b82eed3e59602adbb0e4cecc3
YoshiFrancis <francis.ozua@gmail.com>
2024-05-20 11:20:04 -0500

    remove gitignore modules and add back gitmodules

--- build/Card-Game/CMakeFiles/helper.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for helper.
-# This may be replaced when dependencies are built.
--- build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,3 +0,0 @@
-_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
-  /Users/fozua/Desktop/GameServer/build/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp \
-  /Users/fozua/Desktop/GameServer/build/_deps/catch2-src/src/catch2/../catch2/internal/catch_result_type.hpp
