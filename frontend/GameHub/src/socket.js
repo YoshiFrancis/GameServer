72eac81b4f5cc7561579ddebf278ac1db22df5c3
YoshiFrancis <francis.ozua@gmail.com>
2024-06-09 15:32:04 -0500

    able to connect to web socket server

--- frontend/GameHub/src/socket.js
+++ frontend/GameHub/src/socket.js
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
-export const socket = io('http://localhost:3000');
+export const socket = io('http://localhost:3001', {transports: ['websocket']});
